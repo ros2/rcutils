64d937bf4746e67d026f273f09b754e4e23f7b8b
dhood <dhood@users.noreply.github.com>
2017-09-08 15:30:23 -0700

    Format tokens into buffer as soon as we find them
    Can't dynamically create va_list...

--- src/logging.c
+++ src/logging.c
@@ -20,9 +20,8 @@ extern "C"
 #include <string.h>
 
 #include "rcutils/allocator.h"
-#include "rcutils/logging.h"
 #include "rcutils/get_env.h"
-#include "rcutils/types/string_map.h"
+#include "rcutils/logging.h"
 
 bool g_rcutils_logging_initialized = false;
 const char * g_rcutils_logging_output_format_string = "[{severity}] [{name}]: {message} {line_number}";
@@ -34,7 +33,6 @@ int g_rcutils_logging_severity_threshold = 0;
 void rcutils_logging_initialize()
 {
   if (!g_rcutils_logging_initialized) {
-    printf("format: %s",  g_rcutils_logging_output_format_string);
     g_rcutils_logging_output_handler = &rcutils_logging_console_output_handler;
     g_rcutils_logging_severity_threshold = RCUTILS_LOG_SEVERITY_INFO;
     // Check for the environment variable to disable output formatting
@@ -42,13 +40,8 @@ void rcutils_logging_initialize()
     const char * ret;
     ret = rcutils_get_env("RCUTILS_CONSOLE_OUTPUT_FORMAT", &output_format);
     if (!ret && strcmp(output_format, "") != 0) {
-      fprintf(
-        stderr,
-        "got format string: '%s'\n",
-        output_format);
       g_rcutils_logging_output_format_string = output_format;
     }
-    printf("format: %s",  g_rcutils_logging_output_format_string);
     g_rcutils_logging_initialized = true;
   }
 }
@@ -153,69 +146,64 @@ void rcutils_logging_console_output_handler(
   }
 
   if (!location) {
-    //location = empty_location();
+    // TODO before merge
+    //location = empty_location;
   }
-  rcutils_string_map_t token_printf_type_map = rcutils_get_zero_initialized_string_map();
-  rcutils_ret_t ret;
-  ret = rcutils_string_map_init(&token_printf_type_map, 0, allocator);
-  ret = rcutils_string_map_set(&token_printf_type_map, "message", "%s");
-  ret = rcutils_string_map_set(&token_printf_type_map, "name", "%s");
-  ret = rcutils_string_map_set(&token_printf_type_map, "severity", "%s");
-  ret = rcutils_string_map_set(&token_printf_type_map, "line_number", "%zu");
-
 
   // Process the format string looking for known tokens
   char token_start_delimiter = '{';
   char token_end_delimiter = '}';
-  char substring[1024];
-  char printf_format_string[1024];
-  memset(printf_format_string, '\0', sizeof(printf_format_string));
-  printf("format: %s",  g_rcutils_logging_output_format_string);
+  char output_buffer[1024];
+  memset(output_buffer, '\0', sizeof(output_buffer));
   const char * str = g_rcutils_logging_output_format_string;
   size_t size = strlen(g_rcutils_logging_output_format_string);
 
-  va_list printf_args;
-  va_start (printf_args, format);
   for (size_t i = 0; i < size; ++i) {
-    printf("\n======   %c\n", str[i]);
     if (str[i] == token_start_delimiter) {
-      // look for the end of the token
-      char token[1024];
+      char token[20];
       memset(token, '\0', sizeof(token));
       size_t j = i + 1;
-      while (j < size) {
-        if (str[j] == token_end_delimiter) {
+      // Look for a token end delimeter
+      for (j = i + 1; j < size && str[j] != token_end_delimiter; j++);
+      if (j >= size) {
+          // No end delimiters found; there won't be any more tokens so shortcut the rest of the checking
+          strcat(output_buffer, str + i);
           break;
-        }
-        j++;
       }
-      if (j < size) {
-        size_t token_len = j - i - 1;  // not including delimeters
-        strncpy(token, str + i+1 , token_len);
-        const char * val = rcutils_string_map_get(&token_printf_type_map, token);
-        if (val) {
-          strcat(printf_format_string, val);
-          printf("\nToken: %s\n", token);
+      size_t token_len = j - i - 1;  // not including delimeters
+      strncpy(token, str + i+1 , token_len);
+        char token_buffer[1024];
+        int n = 0;
+        bool known_token = false;
+        if (strcmp("severity", token) == 0)
+        {
+          n = sprintf(token_buffer, "%s", severity_string);
+          known_token = true;
+        }
+        if (!known_token && strcmp("line_number", token) == 0)
+        {
+          n = sprintf(token_buffer, "%zu", location->line_number);
+          known_token = true;
+        }
+        if (!known_token && strcmp("message", token) == 0)
+        {
+          n = sprintf(token_buffer, "%s", message_buffer);
+          known_token = true;
+        }
+        if (!known_token && strcmp("name", token) == 0)
+        {
+          n = sprintf(token_buffer, "%s", name);
+          known_token = true;
+        }
+        if (known_token) {
+          strncat(output_buffer, token_buffer, n);
           i += token_len + 1;  // Skip ahead to avoid re-printing these characters
           continue;
         }
-      }else {
-          // There won't be any more tokens (no end delimiters) so shortcut the rest of the checking
-          strcat(printf_format_string, str + i);
-          break;
-      }
+    }
+        strncat(output_buffer, str + i, 1);
   }
-      strncat(printf_format_string, str + i, 1);
-}
-  printf("\nprintf format string: %s\n", printf_format_string);
-      strncat(printf_format_string, "\n", 1);
-    fprintf(
-      stream, printf_format_string, severity_string, name, message_buffer, location->line_number);
-  /*
-    fprintf(
-      stream, "[%s] [%s]: %s (%s() at %s:%zu)\n", severity_string, name, message_buffer,
-      location->function_name, location->file_name, location->line_number);
-      */
+  fprintf(stream, "%s\n", output_buffer);
 
   if (message_buffer != buffer) {
     allocator.deallocate(message_buffer, allocator.state);
