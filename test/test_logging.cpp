2fc3aa1e26d776e255820dd30b59dc56d34cc94b
dhood <dhood@users.noreply.github.com>
2017-10-10 11:39:50 -0700

    Revert "get_severity_threshold -> get_global_severity_threshold"
    This reverts commit 182b1338bd2233f97d0dcfcfe378af6e93cd5a9a.

--- test/test_logging.cpp
+++ test/test_logging.cpp
@@ -70,7 +70,7 @@ TEST(CLASSNAME(TestLogging, RMW_IMPLEMENTATION), test_logging) {
   rcutils_logging_output_handler_t original_function = rcutils_logging_get_output_handler();
   rcutils_logging_set_output_handler(rcutils_logging_console_output_handler);
 
-  EXPECT_EQ(RCUTILS_LOG_SEVERITY_DEBUG, rcutils_logging_get_global_severity_threshold());
+  EXPECT_EQ(RCUTILS_LOG_SEVERITY_DEBUG, rcutils_logging_get_severity_threshold());
 
   // check all attributes for a debug log message
   rcutils_log_location_t location = {"func", "file", 42u};
@@ -88,9 +88,9 @@ TEST(CLASSNAME(TestLogging, RMW_IMPLEMENTATION), test_logging) {
   EXPECT_EQ("message 11", g_last_log_event.message);
 
   // check global severity threshold
-  int original_severity_threshold = rcutils_logging_get_global_severity_threshold();
-  rcutils_logging_set_global_severity_threshold(RCUTILS_LOG_SEVERITY_INFO);
-  EXPECT_EQ(RCUTILS_LOG_SEVERITY_INFO, rcutils_logging_get_global_severity_threshold());
+  int original_severity_threshold = rcutils_logging_get_severity_threshold();
+  rcutils_logging_set_severity_threshold(RCUTILS_LOG_SEVERITY_INFO);
+  EXPECT_EQ(RCUTILS_LOG_SEVERITY_INFO, rcutils_logging_get_severity_threshold());
   rcutils_log(NULL, RCUTILS_LOG_SEVERITY_DEBUG, "name2", "message %d", 22);
   EXPECT_EQ(1u, g_log_calls);
 
@@ -138,7 +138,7 @@ TEST(CLASSNAME(TestLogging, RMW_IMPLEMENTATION), test_logging) {
     rcutils_logging_get_logger_effective_threshold("rcutils_test_logging_cpp.testing2"));
 
   // restore original state
-  rcutils_logging_set_global_severity_threshold(original_severity_threshold);
+  rcutils_logging_set_severity_threshold(original_severity_threshold);
   rcutils_logging_set_output_handler(original_function);
   g_rcutils_logging_initialized = false;
   EXPECT_FALSE(g_rcutils_logging_initialized);
