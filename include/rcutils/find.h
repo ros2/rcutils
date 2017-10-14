5c5cbbb11d3d4a74f616d5c0aa9803ee6205dc99
dhood <dhood@users.noreply.github.com>
2017-10-13 17:02:58 -0700

    Add rcutils_findn for completeness (not used)

--- include/rcutils/find.h
+++ include/rcutils/find.h
@@ -23,9 +23,33 @@ extern "C"
 #include "rcutils/types.h"
 #include "rcutils/visibility_control.h"
 
+/// Return the first index of a character in a string.
+/**
+ * Search in a string for the first occurence of a delimiter.
+ *
+ * \param[in] str null terminated c string to search
+ * \param[in] delimiter the character to search for
+ * \returns the index of the first occurence of the delimiter if found, or
+ * \returns string_length if the delimiter is not found
+ */
 RCUTILS_PUBLIC
 size_t
 rcutils_find(const char * str, char delimiter);
+
+/// Return the first index of a character in a string of specifed length.
+/**
+ * Identical to rcutils_find_first() but without relying on the string to be a
+ * null terminated c string.
+ *
+ * \param[in] str string to search
+ * \param[in] delimiter the character to search for
+ * \param[in] string_length length of the string to search
+ * \returns the index of the first occurence of the delimiter if found, or
+ * \returns string_length if the delimiter is not found
+ */
+RCUTILS_PUBLIC
+size_t
+rcutils_findn(const char * str, char delimiter, size_t string_length);
 
 /// Return the last index of a character in a string.
 /**
