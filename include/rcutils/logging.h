84ec3b0c5d8632ceebdd1993e4c87cc424cf626e
dhood <dhood@users.noreply.github.com>
2017-11-16 09:59:05 -0800

    Detail tokens

--- include/rcutils/logging.h
+++ include/rcutils/logging.h
@@ -48,12 +48,12 @@ extern bool g_rcutils_logging_initialized;
  * The `RCUTILS_CONSOLE_OUTPUT_FORMAT` environment variable can be used to set
  * the output format of messages logged to the console.
  * Available tokens are:
- *   - file_name
- *   - function_name
- *   - line_number
- *   - message
- *   - name
- *   - severity
+ *   - `file_name`, the full file name of the caller including the path
+ *   - `function_name`, the function name of the caller
+ *   - `line_number`, the line number of the caller
+ *   - `message`, the message string after it has been formatted
+ *   - `name`, the full logger name
+ *   - `severity`, the name of the severity level, e.g. `INFO`
  *
  * The format string can use these tokens by referencing them in curly brackets,
  * e.g. `"[{severity}] [{name}]: {message} ({function_name}() at {file_name}:{line_number})"`.
