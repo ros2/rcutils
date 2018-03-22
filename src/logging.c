9fe41a20d0cb95c70d7122cb5c92158cf5855311
Sagnik Basu <basusagnik1995@gmail.com>
2018-03-22 10:51:05 +0530

    Clean up the fprintf section

--- src/logging.c
+++ src/logging.c
@@ -77,8 +77,8 @@ rcutils_ret_t rcutils_logging_initialize(void)
       }
     }
   } else {
-    fprintf(stderr, "Error getting env. variable"
-      "RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED %s\n", ret_str);
+    fprintf(stderr, "Error getting env variable"
+      "RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED: %s\n", ret_str);
   }
   return rcutils_logging_initialize_with_allocator(rcutils_get_default_allocator());
 }
