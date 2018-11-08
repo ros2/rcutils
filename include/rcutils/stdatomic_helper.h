39887dfd6eaf295f11ab45e78c6bd970abe3ce8e
William Woodall <william@osrfoundation.org>
2018-11-08 14:45:59 -0600

    refactor namespaces after moving files
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- include/rcutils/stdatomic_helper.h
+++ include/rcutils/stdatomic_helper.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef RCL__STDATOMIC_HELPER_H_
-#define RCL__STDATOMIC_HELPER_H_
+#ifndef RCUTILS__STDATOMIC_HELPER_H_
+#define RCUTILS__STDATOMIC_HELPER_H_
 
 #if !defined(_WIN32)
 
@@ -29,101 +29,101 @@
 #include <stdatomic.h>
 #endif
 
-#define rcl_atomic_load(object, out) (out) = atomic_load(object)
+#define rcutils_atomic_load(object, out) (out) = atomic_load(object)
 
-#define rcl_atomic_compare_exchange_strong(object, out, expected, desired) \
+#define rcutils_atomic_compare_exchange_strong(object, out, expected, desired) \
   (out) = atomic_compare_exchange_strong(object, expected, desired)
 
-#define rcl_atomic_exchange(object, out, desired) (out) = atomic_exchange(object, desired)
+#define rcutils_atomic_exchange(object, out, desired) (out) = atomic_exchange(object, desired)
 
-#define rcl_atomic_store(object, desired) atomic_store(object, desired)
+#define rcutils_atomic_store(object, desired) atomic_store(object, desired)
 
 #else  // !defined(_WIN32)
 
 #include "./stdatomic_helper/win32/stdatomic.h"
 
-#define rcl_atomic_load(object, out) rcl_win32_atomic_load(object, out)
+#define rcutils_atomic_load(object, out) rcutils_win32_atomic_load(object, out)
 
-#define rcl_atomic_compare_exchange_strong(object, out, expected, desired) \
-  rcl_win32_atomic_compare_exchange_strong(object, out, expected, desired)
+#define rcutils_atomic_compare_exchange_strong(object, out, expected, desired) \
+  rcutils_win32_atomic_compare_exchange_strong(object, out, expected, desired)
 
-#define rcl_atomic_exchange(object, out, desired) rcl_win32_atomic_exchange(object, out, desired)
+#define rcutils_atomic_exchange(object, out, desired) rcutils_win32_atomic_exchange(object, out, desired)
 
-#define rcl_atomic_store(object, desired) rcl_win32_atomic_store(object, desired)
+#define rcutils_atomic_store(object, desired) rcutils_win32_atomic_store(object, desired)
 
 #endif  // !defined(_WIN32)
 
 static inline bool
-rcl_atomic_load_bool(atomic_bool * a_bool)
+rcutils_atomic_load_bool(atomic_bool * a_bool)
 {
   bool result = false;
-  rcl_atomic_load(a_bool, result);
+  rcutils_atomic_load(a_bool, result);
   return result;
 }
 
 static inline int64_t
-rcl_atomic_load_int64_t(atomic_int_least64_t * a_int64_t)
+rcutils_atomic_load_int64_t(atomic_int_least64_t * a_int64_t)
 {
   int64_t result = 0;
-  rcl_atomic_load(a_int64_t, result);
+  rcutils_atomic_load(a_int64_t, result);
   return result;
 }
 
 static inline uint64_t
-rcl_atomic_load_uint64_t(atomic_uint_least64_t * a_uint64_t)
+rcutils_atomic_load_uint64_t(atomic_uint_least64_t * a_uint64_t)
 {
   uint64_t result = 0;
-  rcl_atomic_load(a_uint64_t, result);
+  rcutils_atomic_load(a_uint64_t, result);
   return result;
 }
 
 static inline uintptr_t
-rcl_atomic_load_uintptr_t(atomic_uintptr_t * a_uintptr_t)
+rcutils_atomic_load_uintptr_t(atomic_uintptr_t * a_uintptr_t)
 {
   uintptr_t result = 0;
-  rcl_atomic_load(a_uintptr_t, result);
+  rcutils_atomic_load(a_uintptr_t, result);
   return result;
 }
 
 static inline bool
-rcl_atomic_compare_exchange_strong_uint_least64_t(
+rcutils_atomic_compare_exchange_strong_uint_least64_t(
   atomic_uint_least64_t * a_uint_least64_t, uint64_t * expected, uint64_t desired)
 {
   bool result;
-  rcl_atomic_compare_exchange_strong(a_uint_least64_t, result, expected, desired);
+  rcutils_atomic_compare_exchange_strong(a_uint_least64_t, result, expected, desired);
   return result;
 }
 
 static inline bool
-rcl_atomic_exchange_bool(atomic_bool * a_bool, bool desired)
+rcutils_atomic_exchange_bool(atomic_bool * a_bool, bool desired)
 {
   bool result;
-  rcl_atomic_exchange(a_bool, result, desired);
+  rcutils_atomic_exchange(a_bool, result, desired);
   return result;
 }
 
 static inline int64_t
-rcl_atomic_exchange_int64_t(atomic_int_least64_t * a_int64_t, int64_t desired)
+rcutils_atomic_exchange_int64_t(atomic_int_least64_t * a_int64_t, int64_t desired)
 {
   int64_t result;
-  rcl_atomic_exchange(a_int64_t, result, desired);
+  rcutils_atomic_exchange(a_int64_t, result, desired);
   return result;
 }
 
 static inline uint64_t
-rcl_atomic_exchange_uint64_t(atomic_uint_least64_t * a_uint64_t, uint64_t desired)
+rcutils_atomic_exchange_uint64_t(atomic_uint_least64_t * a_uint64_t, uint64_t desired)
 {
   uint64_t result;
-  rcl_atomic_exchange(a_uint64_t, result, desired);
+  rcutils_atomic_exchange(a_uint64_t, result, desired);
   return result;
 }
 
 static inline uintptr_t
-rcl_atomic_exchange_uintptr_t(atomic_uintptr_t * a_uintptr_t, uintptr_t desired)
+rcutils_atomic_exchange_uintptr_t(atomic_uintptr_t * a_uintptr_t, uintptr_t desired)
 {
   uintptr_t result;
-  rcl_atomic_exchange(a_uintptr_t, result, desired);
+  rcutils_atomic_exchange(a_uintptr_t, result, desired);
   return result;
 }
 
-#endif  // RCL__STDATOMIC_HELPER_H_
+#endif  // RCUTILS__STDATOMIC_HELPER_H_
