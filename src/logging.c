b19b3afcc4b453d7470d217a0e4ad84d1b8a1ed1
Dan Rose <dan@digilabs.io>
2019-07-13 14:25:56 -0500

    default RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED to 1
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- src/logging.c
+++ src/logging.c
@@ -75,7 +75,7 @@ bool g_rcutils_logging_severities_map_valid = false;
 
 int g_rcutils_logging_default_logger_level = 0;
 
-bool g_force_stdout_line_buffered = false;
+bool g_force_stdout_line_buffered = true;
 bool g_stdout_flush_failure_reported = false;
 
 enum rcutils_colorized_output g_colorized_output = RCUTILS_COLORIZED_OUTPUT_AUTO;
@@ -103,12 +103,12 @@ rcutils_ret_t rcutils_logging_initialize_with_allocator(rcutils_allocator_t allo
     const char * ret_str = rcutils_get_env("RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED", &line_buffered);
 
     if (NULL == ret_str) {
-      if (strcmp(line_buffered, "1") == 0) {
-        g_force_stdout_line_buffered = true;
+      if (strcmp(line_buffered, "0") == 0) {
+        g_force_stdout_line_buffered = false;
       } else if (strcmp(line_buffered, "0") != 0 && strcmp(line_buffered, "") != 0) {
         fprintf(stderr,
           "Warning: unexpected value [%s] specified for RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED. "
-          "Default value 0 will be used. Valid values are 1 or 0.\n",
+          "Default value 1 will be used. Valid values are 1 or 0.\n",
           line_buffered);
       }
     } else {
