832b299b35ae7dc522eca0fd7d33352c116896f2
Jorge J. Perez <jjperez@ekumenlabs.com>
2020-03-02 17:43:20 -0300

    Increase huge number fail test
    Signed-off-by: Jorge J. Perez <jjperez@ekumenlabs.com>

--- test/test_array_list.cpp
+++ test/test_array_list.cpp
@@ -369,7 +369,7 @@ TEST_F(ArrayListTest, init_list_bad_allocator_fail) {
 }
 
 TEST_F(ArrayListTest, init_list_huge_fail) {
-  rcutils_ret_t ret = rcutils_array_list_init(&list, 9999999999, sizeof(uint32_t), &allocator);
+  rcutils_ret_t ret = rcutils_array_list_init(&list, 9999999999999, sizeof(uint32_t), &allocator);
   EXPECT_EQ(RCUTILS_RET_BAD_ALLOC, ret) << rcutils_get_error_string().str;
 }
 
