35f29850064e0c33a4063cbc947ebbfeada11dba
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2020-04-01 17:53:36 +0200

    Included utils to load, unload and get symbols from shared libraries (#215)
    * Included utils to load, unload and get symbols from shared libraries
    
    Shared library working on Windows
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * platform independent code moved outside of the conditional blocks
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Included feedback
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    Added shared library test
    
    fix return error on shared library for windows
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Exported CMake_DL_LIBS
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Included more feedback
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Improved tests
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Fixed argument checks in rcutils_has_symbol
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * fixed naming rcutils_unload_library to rcutils_unload_shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Added feedback
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * deallocated memory when open fails
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Added null after deallocate memory
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Updated doc shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Improved error handling
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Fixed uncrustify and cppcheck
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Added allocator argument to rcutils_load_shared_library funcion
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * checked inputs and deduplicate code
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * added more test to shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * fixed unscrustify test_shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Fixed docs
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Update include/rcutils/shared_library.h
    
    Co-Authored-By: William Woodall <william+github@osrfoundation.org>
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Improved docs
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * update shared library docs
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Avoided leak when a library is loaded two time without unload
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * changed the order to deallocate memory
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Added extension for different platforms in test_shared_libraries
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * fixed ccplint
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Fixed dummy library name
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Right library name in windows in test_shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * preprocessor directives not indented
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Added rcutils_get_platform_library_name fucntion to shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * fixed rcutils_snprintf buffer size
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Added target_compile_definitions for dummy_shared_library
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * defined len of str in rcutils_get_platform_library_name
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * Removed limitation on the path size for the library name
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    * fixed condition len string in rcutils_get_platform_library_name
    
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    
    Co-authored-by: William Woodall <william+github@osrfoundation.org>

---
+++ src/shared_library.c
@@ -0,0 +1,197 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "rcutils/error_handling.h"
+#include "rcutils/shared_library.h"
+#include "rcutils/strdup.h"
+
+rcutils_shared_library_t
+rcutils_get_zero_initialized_shared_library(void)
+{
+  rcutils_shared_library_t zero_initialized_shared_library;
+  zero_initialized_shared_library.library_path = NULL;
+  zero_initialized_shared_library.lib_pointer = NULL;
+  zero_initialized_shared_library.allocator = rcutils_get_zero_initialized_allocator();
+  return zero_initialized_shared_library;
+}
+
+rcutils_ret_t
+rcutils_load_shared_library(
+  rcutils_shared_library_t * lib,
+  const char * library_path,
+  rcutils_allocator_t allocator)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(lib, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(library_path, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ALLOCATOR(&allocator, return RCUTILS_RET_INVALID_ARGUMENT);
+
+  if (lib->library_path != NULL) {
+    lib->allocator.deallocate(lib->library_path, lib->allocator.state);
+  }
+
+  lib->allocator = allocator;
+
+  lib->library_path = rcutils_strdup(library_path, lib->allocator);
+  if (NULL == lib->library_path) {
+    RCUTILS_SET_ERROR_MSG("unable to allocate memory");
+    return RCUTILS_RET_BAD_ALLOC;
+  }
+
+#ifndef _WIN32
+  lib->lib_pointer = dlopen(lib->library_path, RTLD_LAZY);
+  if (!lib->lib_pointer) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING("LoadLibrary error: %s", dlerror());
+#else
+  lib->lib_pointer = LoadLibrary(lib->library_path);
+  if (!lib->lib_pointer) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING("LoadLibrary error: %lu", GetLastError());
+#endif  // _WIN32
+    lib->allocator.deallocate(lib->library_path, lib->allocator.state);
+    lib->library_path = NULL;
+    return RCUTILS_RET_ERROR;
+  }
+  return RCUTILS_RET_OK;
+}
+
+void *
+rcutils_get_symbol(const rcutils_shared_library_t * lib, const char * symbol_name)
+{
+  if (!lib || !lib->lib_pointer || (symbol_name == NULL)) {
+    RCUTILS_SET_ERROR_MSG("invalid inputs arguments");
+    return NULL;
+  }
+
+#ifndef _WIN32
+  void * lib_symbol = dlsym(lib->lib_pointer, symbol_name);
+  char * error = dlerror();
+  if (error != NULL) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING(
+      "Error getting the symbol '%s'. Error '%s'",
+      symbol_name, error);
+    return NULL;
+  }
+#else
+  void * lib_symbol = GetProcAddress(lib->lib_pointer, symbol_name);
+  if (lib_symbol == NULL) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING(
+      "Error getting the symbol '%s'. Error '%d'",
+      symbol_name, GetLastError());
+    return NULL;
+  }
+#endif  // _WIN32
+  if (!lib_symbol) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING(
+      "symbol '%s' does not exist in the library '%s'",
+      symbol_name, lib->library_path);
+    return NULL;
+  }
+  return lib_symbol;
+}
+
+bool
+rcutils_has_symbol(const rcutils_shared_library_t * lib, const char * symbol_name)
+{
+  if (!lib || !lib->lib_pointer || symbol_name == NULL) {
+    return false;
+  }
+
+#ifndef _WIN32
+  // the correct way to test for an error is to call dlerror() to clear any old error conditions,
+  // then call dlsym(), and then call dlerror() again, saving its return value into a variable,
+  // and check whether this saved value is not NULL.
+  dlerror(); /* Clear any existing error */
+  void * lib_symbol = dlsym(lib->lib_pointer, symbol_name);
+  return dlerror() == NULL && lib_symbol != 0;
+#else
+  void * lib_symbol = GetProcAddress(lib->lib_pointer, symbol_name);
+  return GetLastError() == 0 && lib_symbol != 0;
+#endif  // _WIN32
+}
+
+rcutils_ret_t
+rcutils_unload_shared_library(rcutils_shared_library_t * lib)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(lib, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(lib->lib_pointer, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(lib->library_path, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ALLOCATOR(&lib->allocator, return RCUTILS_RET_INVALID_ARGUMENT);
+
+  rcutils_ret_t ret = RCUTILS_RET_OK;
+#ifndef _WIN32
+  // The function dlclose() returns 0 on success, and nonzero on error.
+  int error_code = dlclose(lib->lib_pointer);
+  if (error_code) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING("dlclose error: %s", dlerror());
+#else
+  // If the function succeeds, the return value is nonzero.
+  int error_code = FreeLibrary(lib->lib_pointer);
+  if (!error_code) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING("FreeLibrary error: %lu", GetLastError());
+#endif  // _WIN32
+    ret = RCUTILS_RET_ERROR;
+  }
+
+  lib->allocator.deallocate(lib->library_path, lib->allocator.state);
+  lib->library_path = NULL;
+  lib->lib_pointer = NULL;
+  lib->allocator = rcutils_get_zero_initialized_allocator();
+  return ret;
+}
+
+rcutils_ret_t
+rcutils_get_platform_library_name(
+  const char * library_name,
+  char * library_name_platform,
+  unsigned int buffer_size)
+{
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(library_name, RCUTILS_RET_INVALID_ARGUMENT);
+  RCUTILS_CHECK_ARGUMENT_FOR_NULL(library_name_platform, RCUTILS_RET_INVALID_ARGUMENT);
+
+  int written = 0;
+
+#ifdef __linux__
+  if (buffer_size >= (strlen(library_name) + 7)) {
+    written = rcutils_snprintf(
+      library_name_platform, strlen(library_name) + 7, "lib%s.so", library_name);
+  }
+#elif __APPLE__
+  if (buffer_size >= (strlen(library_name) + 10)) {
+    written = rcutils_snprintf(
+      library_name_platform, strlen(library_name) + 10, "lib%s.dylib", library_name);
+  }
+#elif _WIN32
+  if (buffer_size >= (strlen(library_name) + 7)) {
+    written = rcutils_snprintf(
+      library_name_platform, strlen(library_name) + 5, "%s.dll", library_name);
+  }
+#endif
+  if (written < 0) {
+    RCUTILS_SET_ERROR_MSG_WITH_FORMAT_STRING(
+      "failed to format library name: '%s'\n",
+      library_name);
+    return RCUTILS_RET_ERROR;
+  }
+  return RCUTILS_RET_OK;
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ test/test_shared_library.cpp
@@ -0,0 +1,155 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "./allocator_testing_utils.h"
+
+#include "rcutils/allocator.h"
+#include "rcutils/error_handling.h"
+#include "rcutils/shared_library.h"
+
+#include "rcutils/get_env.h"
+
+class TestSharedLibrary : public ::testing::Test
+{
+protected:
+  void SetUp() final
+  {
+    // Reset rcutil error global state in case a previously
+    // running test has failed.
+    rcutils_reset_error();
+    lib = rcutils_get_zero_initialized_shared_library();
+  }
+  rcutils_shared_library_t lib;
+  char library_path[1024];
+};
+
+TEST_F(TestSharedLibrary, basic_load) {
+  rcutils_ret_t ret;
+
+  // checking rcutils_get_zero_initialized_shared_library
+  ASSERT_STRNE(lib.library_path, "");
+  EXPECT_TRUE(lib.lib_pointer == NULL);
+
+  ret = rcutils_get_platform_library_name("dummy_shared_library", library_path, 1024);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // getting shared library
+  ret = rcutils_load_shared_library(&lib, library_path, rcutils_get_default_allocator());
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // unload shared_library
+  ret = rcutils_unload_shared_library(&lib);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // checking if we have unloaded and freed memory
+  ASSERT_STRNE(lib.library_path, "");
+  EXPECT_TRUE(lib.lib_pointer == NULL);
+}
+
+TEST_F(TestSharedLibrary, load_two_times) {
+  rcutils_ret_t ret;
+
+  ret = rcutils_get_platform_library_name("dummy_shared_library", library_path, 1024);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // getting shared library
+  ret = rcutils_load_shared_library(&lib, library_path, rcutils_get_default_allocator());
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // getting shared library
+  ret = rcutils_load_shared_library(&lib, library_path, rcutils_get_default_allocator());
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // unload shared_library
+  ret = rcutils_unload_shared_library(&lib);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+}
+
+TEST_F(TestSharedLibrary, error_load) {
+  rcutils_ret_t ret;
+
+  rcutils_shared_library_t lib_empty;
+  ret = rcutils_load_shared_library(&lib_empty, NULL, rcutils_get_zero_initialized_allocator());
+  ASSERT_EQ(RCUTILS_RET_INVALID_ARGUMENT, ret);
+
+  lib_empty = rcutils_get_zero_initialized_shared_library();
+  ret = rcutils_load_shared_library(&lib_empty, NULL, rcutils_get_zero_initialized_allocator());
+  ASSERT_EQ(RCUTILS_RET_INVALID_ARGUMENT, ret);
+
+  ret = rcutils_get_platform_library_name("dummy_shared_library", library_path, 1024);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  ret = rcutils_load_shared_library(
+    &lib_empty,
+    library_path, rcutils_get_zero_initialized_allocator());
+  ASSERT_EQ(RCUTILS_RET_INVALID_ARGUMENT, ret);
+}
+
+TEST_F(TestSharedLibrary, error_unload) {
+  rcutils_ret_t ret;
+
+  ret = rcutils_get_platform_library_name("dummy_shared_library", library_path, 1024);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  ret = rcutils_load_shared_library(&lib, library_path, rcutils_get_default_allocator());
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // unload shared_library
+  ret = rcutils_unload_shared_library(&lib);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // unload again shared_library
+  ret = rcutils_unload_shared_library(&lib);
+  ASSERT_EQ(RCUTILS_RET_INVALID_ARGUMENT, ret);
+}
+
+TEST_F(TestSharedLibrary, error_symbol) {
+  bool is_symbol = rcutils_has_symbol(&lib, "symbol");
+  EXPECT_TRUE(is_symbol == false);
+
+  void * symbol = rcutils_get_symbol(&lib, "print_name");
+  EXPECT_TRUE(symbol == NULL);
+}
+
+TEST_F(TestSharedLibrary, basic_symbol) {
+  void * symbol;
+  bool ret;
+
+  symbol = rcutils_get_symbol(nullptr, "symbol");
+  EXPECT_TRUE(symbol == NULL);
+
+  ret = rcutils_has_symbol(nullptr, "symbol");
+  EXPECT_FALSE(ret);
+
+  ret = rcutils_get_platform_library_name("dummy_shared_library", library_path, 1024);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  // getting shared library
+  ret = rcutils_load_shared_library(&lib, library_path, rcutils_get_default_allocator());
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+
+  symbol = rcutils_get_symbol(&lib, "symbol");
+  EXPECT_TRUE(symbol == NULL);
+
+  symbol = rcutils_get_symbol(&lib, "print_name");
+  EXPECT_TRUE(symbol != NULL);
+
+  // unload shared_library
+  ret = rcutils_unload_shared_library(&lib);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+}
