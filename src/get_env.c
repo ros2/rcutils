1973b4a32b6ff2e6452aa48a622a77aa31d116b0
Suyash Behera <Sne9x@outlook.com>
2020-04-27 18:39:40 +0530

    update rcutils_get_env to always use getenv (#237)
    * update rcutils_get_env to use getenv for all platforms
    
    Signed-off-by: Suyash458 <suyash.behera458@gmail.com>

--- src/get_env.c
+++ src/get_env.c
@@ -23,13 +23,8 @@ extern "C"
 #include "rcutils/get_env.h"
 
 #ifdef _WIN32
-# include <errno.h>
-// all environment variables live together in a single memory block
-// which has a limit of 32767 characters
-# define WINDOWS_ENV_BUFFER_SIZE 32767
-static char __env_buffer[WINDOWS_ENV_BUFFER_SIZE];
-#endif  // _WIN32
-
+#pragma warning(disable : 4996)
+#endif
 
 const char *
 rcutils_get_env(const char * env_name, const char ** env_value)
@@ -40,28 +35,13 @@ rcutils_get_env(const char * env_name, const char ** env_value)
   if (NULL == env_value) {
     return "argument env_value is null";
   }
-  *env_value = NULL;
-#ifdef _WIN32
-  size_t required_size;
-  errno_t ret = getenv_s(&required_size, __env_buffer, sizeof(__env_buffer), env_name);
-  switch (ret) {
-    case 0:
-      __env_buffer[WINDOWS_ENV_BUFFER_SIZE - 1] = '\0';
-      *env_value = __env_buffer;
-      break;
-    case EINVAL:
-      return "invalid arguments when reading environment variable";
-    case ERANGE:
-      return "insufficient buffer size to read environment variable";
-    default:
-      return "unknown error code reading environment variable";
-  }
-#else
+
+  // TODO(Suyash458): getenv is deprecated on Windows; consider using getenv_s instead
   *env_value = getenv(env_name);
+
   if (NULL == *env_value) {
     *env_value = "";
   }
-#endif  // _WIN32
   return NULL;
 }
 
