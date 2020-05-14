b1ec2c6b37203a880e34b788ea0dd22b7321dae4
Scott K Logan <logans@cottsay.net>
2020-05-13 17:16:50 -0700

    Add rcutils_set_env function
    This change adds a simple function for setting or un-setting an
    environment variable for the process.
    
    The existing environment variable functions in rcutils are under a
    header called `get_env.h`. It might be good to deprecate that header and
    move the functionality to the more generically-named `env.h`.
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

---
+++ include/rcutils/env.h
@@ -0,0 +1,47 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RCUTILS__ENV_H_
+#define RCUTILS__ENV_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+
+#include "rcutils/macros.h"
+#include "rcutils/visibility_control.h"
+
+/// Set or un-set a process-scoped environment variable.
+/**
+ * This function modifies the environment variables for the current process.
+ *
+ * \param[in] env_name Name of the environment variable to modify.
+ * \param[in] env_value Value to set the environment variable to, or NULL to un-set.
+ * \return `True` if success
+ * \return `False` if env_name is invalid or NULL
+ * \return `False` on failure
+ */
+RCUTILS_PUBLIC
+RCUTILS_WARN_UNUSED
+bool
+rcutils_set_env(const char * env_name, const char * env_value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // RCUTILS__ENV_H_
