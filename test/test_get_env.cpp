3b25d1f44074915cfff88cc11e6d382d1a190044
Stephen Brawner <brawner@gmail.com>
2020-06-03 17:23:40 -0700

    PR Fixup
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- test/test_get_env.cpp
+++ test/test_get_env.cpp
@@ -50,15 +50,22 @@ TEST(TestGetEnv, test_get_env) {
 
 TEST(TestGetEnv, test_get_home) {
   EXPECT_STRNE(NULL, rcutils_get_home_dir());
-
   const char * home = NULL;
-  const char * ret = rcutils_get_env("HOME", &home);
-  EXPECT_EQ(NULL, ret);
 
 #ifdef _WIN32
+  // Assert pre-condition that USERPROFILE is defined
+  const char * ret = rcutils_get_env("USERPROFILE", &home);
+  ASSERT_EQ(NULL, ret);
+
+  // Check USERPROFILE is not defined
   EXPECT_TRUE(rcutils_set_env("USERPROFILE", NULL));
   EXPECT_EQ(NULL, rcutils_get_home_dir());
 #else
+  // Assert pre-condition that HOME is defined
+  const char * ret = rcutils_get_env("HOME", &home);
+  ASSERT_EQ(NULL, ret);
+
+  // Check HOME is not defined
   EXPECT_TRUE(rcutils_set_env("HOME", NULL));
   EXPECT_EQ(NULL, rcutils_get_home_dir());
 #endif
