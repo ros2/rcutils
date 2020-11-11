8458bba7280a491954d0486ecfac7e865d0203f3
Chris Lalancette <clalancette@openrobotics.org>
2020-11-11 14:58:49 +0000

    Disable a Windows platform warning.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- src/logging.c
+++ src/logging.c
@@ -26,7 +26,18 @@ extern "C"
 
 #ifdef _WIN32
 # include <io.h>
+// When building with MSVC 19.28.29333.0 on Windows 10 (as of 2020-11-11),
+// there appears to be a problem with winbase.h (which is included by
+// Windows.h).  In particular, warnings of the form:
+//
+// warning C5105: macro expansion producing 'defined' has undefined behavior
+//
+// See https://developercommunity.visualstudio.com/content/problem/695656/wdk-and-sdk-are-not-compatible-with-experimentalpr.html
+// for more information.  For now disable that warning when including windows.h
+# pragma warning(push)
+# pragma warning(disable : 5105)
 # include <windows.h>
+# pragma warning(pop)
 #else
 # include <unistd.h>
 #endif
