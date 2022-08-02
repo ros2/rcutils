846905073b9d6d4286f9b4c0378a5cc37889133b
Chris Lalancette <clalancette@openrobotics.org>
2022-08-02 19:28:35 +0000

    Get rid of unnecessary and unused RMW switching for logging tests.
    We had some of the infrastructure in place to run tests with
    different RMWs, but we were never using it.  Additionally, it
    doesn't make a huge amount of sense to use different RMWs
    at the rcutils test layer; it is below the RMW.  Just remove
    the infrastructure.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- test/test_logging_custom_env.cpp
+++ test/test_logging_custom_env.cpp
@@ -22,14 +22,7 @@
 #include "osrf_testing_tools_cpp/scope_exit.hpp"
 #include "rcutils/logging.h"
 
-#ifdef RMW_IMPLEMENTATION
-# define CLASSNAME_(NAME, SUFFIX) NAME ## __ ## SUFFIX
-# define CLASSNAME(NAME, SUFFIX) CLASSNAME_(NAME, SUFFIX)
-#else
-# define CLASSNAME(NAME, SUFFIX) NAME
-#endif
-
-TEST(CLASSNAME(TestLoggingCustomEnv, RMW_IMPLEMENTATION), test_logging) {
+TEST(TestLoggingCustomEnv, test_logging) {
   EXPECT_FALSE(g_rcutils_logging_initialized);
   ASSERT_EQ(RCUTILS_RET_OK, rcutils_logging_initialize());
   OSRF_TESTING_TOOLS_CPP_SCOPE_EXIT(
@@ -56,7 +49,7 @@ TEST(CLASSNAME(TestLoggingCustomEnv, RMW_IMPLEMENTATION), test_logging) {
   EXPECT_EQ(RCUTILS_RET_OK, rcutils_char_array_fini(&msg_buf));
 }
 
-TEST(CLASSNAME(TestLoggingCustomEnv, RMW_IMPLEMENTATION), test_logging_with_buffering_issues) {
+TEST(TestLoggingCustomEnv, test_logging_with_buffering_issues) {
   auto mock = mocking_utils::patch("lib:rcutils", setvbuf, [](auto && ...) {return -1;});
   EXPECT_FALSE(g_rcutils_logging_initialized);
   EXPECT_EQ(RCUTILS_RET_ERROR, rcutils_logging_initialize());
