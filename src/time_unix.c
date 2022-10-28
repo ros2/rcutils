e47ec703a1dcc2d9853bc20ab94d3aaa38b32841
Pablo Garrido <pablogs9@gmail.com>
2022-10-28 09:11:00 +0200

    time_unix: uses ZEPHYR_VERSION_CODE instead (#390)
    of KERNELVERSION, since zephyr from 2.7 does not use it anymore.
    
    Signed-off-by: Felipe Neves <felipe.neves@linaro.org>
    
    Fix
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>
    
    Update
    
    Signed-off-by: Pablo Garrido <pablogs9@gmail.com>

--- src/time_unix.c
+++ src/time_unix.c
@@ -30,7 +30,12 @@ extern "C"
 #include <math.h>
 
 #if defined(__ZEPHYR__)
-#include <posix/time.h>  //  Points to Zephyr toolchain posix time implementation
+#include <version.h>
+#if ZEPHYR_VERSION_CODE >= ZEPHYR_VERSION(3, 1, 0)
+#include <zephyr/posix/time.h>  //  Points to Zephyr toolchain posix time implementation
+#else
+#include <time.h>
+#endif // ZEPHYR_VERSION_CODE >= ZEPHYR_VERSION(3, 1, 0)
 #else
 #include <time.h>
 #endif  //  defined(__ZEPHYR__)
