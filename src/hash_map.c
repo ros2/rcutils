1a072272ace9e0ba9d267a532d1621e15952f5e9
Chris Lalancette <clalancette@gmail.com>
2023-04-06 17:43:42 -0400

    Update src/hash_map.c
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>
    
    Co-authored-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- src/hash_map.c
+++ src/hash_map.c
@@ -233,11 +233,10 @@ static rcutils_ret_t hash_map_check_and_grow_map(rcutils_hash_map_t * hash_map)
 // Modified from http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
 static size_t next_power_of_two(size_t v)
 {
+  size_t shf;
   v--;
-  size_t shf = 1;
-  v |= v >> 1;
-  for (size_t i = 0; i < sizeof(size_t); ++i) {
-    shf <<= 1;
+  for (size_t i = 0; i <= sizeof(size_t); ++i) {
+    shf = (1 << i);
     v |= v >> shf;
   }
   v++;
