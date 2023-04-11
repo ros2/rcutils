dbd3ff46d7de4696a074a37a5cb698e696b78839
Chris Lalancette <clalancette@gmail.com>
2023-04-10 20:43:40 -0400

    Calculate the next power-of-two for the user in hash_map_init. (#420)
    * Calculate the next power-of-two for the user in hash_map_init.
    
    That is, we allow the user to specify non powers of two,
    and then we do the calculation for them.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>
    Co-authored-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    Co-authored-by: methylDragon <methylDragon@gmail.com>

--- src/hash_map.c
+++ src/hash_map.c
@@ -230,6 +230,19 @@ static rcutils_ret_t hash_map_check_and_grow_map(rcutils_hash_map_t * hash_map)
   return ret;
 }
 
+// Modified from http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+static size_t next_power_of_two(size_t v)
+{
+  size_t shf = 0;
+  v--;
+  for (size_t i = 0; shf < sizeof(size_t) * 4; ++i) {
+    shf = (((size_t) 1) << i);
+    v |= v >> shf;
+  }
+  v++;
+  return v > 1 ? v : 1;
+}
+
 rcutils_ret_t
 rcutils_hash_map_init(
   rcutils_hash_map_t * hash_map,
@@ -255,11 +268,10 @@ rcutils_hash_map_init(
     return RCUTILS_RET_INVALID_ARGUMENT;
   }
 
-  // Due to an optimization we use during lookup, we can currently only handle power-of-two
-  // capacities.  Enforce that here.
+  // Due to an optimization we use during lookup, the capacity must be a power-of-two.
+  // If the user passed us something that is not power-of-two, round up to the next power-of-two
   if ((initial_capacity & (initial_capacity - 1)) != 0) {
-    RCUTILS_SET_ERROR_MSG("This hashmap only works with power-of-two capacities");
-    return RCUTILS_RET_INVALID_ARGUMENT;
+    initial_capacity = next_power_of_two(initial_capacity);
   }
 
   hash_map->impl = allocator->allocate(sizeof(rcutils_hash_map_impl_t), allocator->state);
