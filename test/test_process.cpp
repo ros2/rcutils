96d5cc47b3f4dda0fcb21ad5e908cea9b7c088e4
Alejandro Hernández Cordero <ahcorde@gmail.com>
2025-04-30 15:03:58 +0200

    Clean memory in test_process.cpp (#495)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    (cherry picked from commit 072ba18c3b0f720d58057d2cd900e907abd7863d)
    
    # Conflicts:
    #	test/test_join.cpp
    #	test/test_process.cpp

--- test/test_process.cpp
+++ test/test_process.cpp
@@ -42,3 +42,86 @@ TEST(TestProcess, test_get_executable_name) {
   EXPECT_STREQ("test_process", exec_name);
   allocator.deallocate(exec_name, allocator.state);
 }
+<<<<<<< HEAD
+=======
+
+TEST(TestProcess, test_process_creation) {
+  rcutils_allocator_t allocator = rcutils_get_default_allocator();
+  rcutils_allocator_t bad_allocator = rcutils_get_zero_initialized_allocator();
+  rcutils_allocator_t time_bomb_allocator = get_time_bomb_allocator();
+  rcutils_string_array_t args = rcutils_get_zero_initialized_string_array();
+  rcutils_process_t * process = NULL;
+  rcutils_ret_t ret = RCUTILS_RET_OK;
+  int exit_code = 42;
+
+  ret = rcutils_string_array_init(&args, 4, &allocator);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+  args.data[0] = strdup(g_cmake_command);
+  args.data[1] = strdup("-E");
+  args.data[2] = strdup("echo");
+  args.data[3] = strdup("");
+
+  EXPECT_EQ(nullptr, rcutils_start_process(NULL, &allocator));
+  rcutils_reset_error();
+
+  EXPECT_EQ(nullptr, rcutils_start_process(&args, NULL));
+  rcutils_reset_error();
+
+  EXPECT_EQ(nullptr, rcutils_start_process(&args, &bad_allocator));
+  rcutils_reset_error();
+
+  set_time_bomb_allocator_calloc_count(time_bomb_allocator, 0);
+  set_time_bomb_allocator_malloc_count(time_bomb_allocator, 0);
+  EXPECT_EQ(nullptr, rcutils_start_process(&args, &time_bomb_allocator));
+
+  process = rcutils_start_process(&args, &allocator);
+  EXPECT_NE(nullptr, process);
+
+  ret = rcutils_process_wait(process, &exit_code);
+  EXPECT_EQ(RCUTILS_RET_OK, ret);
+  EXPECT_EQ(0, exit_code);
+
+  rcutils_process_close(process);
+
+  ret = rcutils_string_array_fini(&args);
+
+  // cmake -E cat "file with space.txt" (returns 0)
+  ret = rcutils_string_array_resize(&args, 4);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+  args.data[0] = strdup(g_cmake_command);
+  args.data[1] = strdup("-E");
+  args.data[2] = strdup("cat");
+  args.data[3] = strdup("file with space.txt");
+
+  process = rcutils_start_process(&args, &allocator);
+  EXPECT_NE(nullptr, process);
+
+  ret = rcutils_process_wait(process, &exit_code);
+  EXPECT_EQ(RCUTILS_RET_OK, ret);
+  EXPECT_EQ(0, exit_code);
+
+  rcutils_process_close(process);
+
+  ret = rcutils_string_array_fini(&args);
+
+  // cmake -E false (returns 1)
+  ret = rcutils_string_array_resize(&args, 3);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+  args.data[0] = strdup(g_cmake_command);
+  args.data[1] = strdup("-E");
+  allocator.deallocate(args.data[2], &allocator.state);
+  args.data[2] = strdup("false");
+
+  process = rcutils_start_process(&args, &allocator);
+  EXPECT_NE(nullptr, process);
+
+  ret = rcutils_process_wait(process, &exit_code);
+  EXPECT_EQ(RCUTILS_RET_OK, ret);
+  EXPECT_EQ(1, exit_code);
+
+  rcutils_process_close(process);
+
+  ret = rcutils_string_array_fini(&args);
+  ASSERT_EQ(RCUTILS_RET_OK, ret);
+}
+>>>>>>> 072ba18 (Clean memory in test_process.cpp (#495))
