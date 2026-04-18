ceb59e11d6a81bcaf6faa2cc49342c40b0b89614
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2026-04-18 15:22:03 +0200

    Change fault injection count initialization method
    fix #553 
    
    Signed-off-by: mosfet80 <10235105+mosfet80@users.noreply.github.com>

--- src/testing/fault_injection.c
+++ src/testing/fault_injection.c
@@ -16,8 +16,7 @@
 
 #include "rcutils/stdatomic_helper.h"
 
-static atomic_int_least64_t g_rcutils_fault_injection_count = ATOMIC_VAR_INIT(-1);
-
+static atomic_int_least64_t g_rcutils_fault_injection_count = ATOMIC_FLAG_INIT;
 void rcutils_fault_injection_set_count(int_least64_t count)
 {
   rcutils_atomic_store(&g_rcutils_fault_injection_count, count);
